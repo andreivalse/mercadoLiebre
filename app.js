fafbc3b91ebc51a224a1c8453db4494b5b4e28e5
Andreiev Valencia <andreivalse@gmail.com>
2022-06-30 20:34:38 -0500

    Se agrega proyecto a git

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path= require('path');
+const PORT= process.env.PORT || 2000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
+app.listen(PORT, () => console.log('http://localhost:'+ PORT));
